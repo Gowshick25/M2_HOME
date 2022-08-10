9a49f4621fe62ad91be1a7350a829f79eebecf34
Gowshick25 <103167849+Gowshick25@users.noreply.github.com>
2022-08-10 14:56:57 +0530

    Add files via upload

---
+++ maven-jenkins/src/main/java/com/example/maven_jenkins/App.java
@@ -0,0 +1,10 @@
+package com.example.maven_jenkins;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World built with jenkins" );
+    }
+}
